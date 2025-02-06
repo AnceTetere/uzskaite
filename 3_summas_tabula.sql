c42b57cf60b2ccf64e98e0a14d54103932ac7d5f
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2025-02-06 21:55:52 +0200

    Create 3_summas_tabula.sql

---
+++ 3_summas_tabula.sql
@@ -0,0 +1,34 @@
+SELECT COUNT(*)
+FROM [dbo].[R summas_uzskaitei_LV]; -- 33 074 rindas
+
+-- Atribūts 1: ID_num
+--1 Apskata aili ID_num
+SELECT [ID_num] 
+FROM [dbo].[R summas_uzskaitei_LV];
+
+--2 Pārbauda, cik rakstuzīmes plats ir atribūta resnākais gals
+SELECT [ID_num], LEN(ID_num) AS platums 
+FROM 
+WHERE LEN(ID_num) > 7
+
+--3 Pārtaisa un definē aili
+ALTER TABLE [dbo].[R summas_uzskaitei_LV] ALTER COLUMN [ID_num] VARCHAR(7) NOT NULL; -- ID vērtība nedrīkst būt tukša
+
+-- Atribūts 2: Summa
+--4 Apskata aili Summa
+SELECT [Summa]
+FROM [dbo].[R summas_uzskaitei_LV];
+
+--5 Definē atribūtu
+ALTER TABLE [dbo].[R summas_uzskaitei_LV] ALTER COLUMN [Summa] float(8); 
+
+--6 Izveido tukšu tabulu
+CREATE TABLE summas_LV (
+  ID_num varchar(7) NOT NULL,
+  summa float(8));
+
+--7 Pārņem noformētos datus uz jauno tabulu
+INSERT INTO summas_LV
+SELECT *
+FROM [dbo].[R summas_uzskaitei_LV]
+--33 074 rindas
