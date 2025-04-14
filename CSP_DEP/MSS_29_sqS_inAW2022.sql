f030e361cdd82efc52f90598503eaf82492387ca
AnceTetere <ance.tetere@outlook.com>
2025-04-14 12:51:15 +0300

    sqS in AW2022.

---
+++ CSP_DEP/MSS_29_sqS_inAW2022.sql
@@ -0,0 +1,18 @@
+USE AdventureWorks2022
+GO
+
+SELECT * FROM Sales.SalesOrderDetail;
+SELECT * FROM Production.Product
+WHERE Name = 'Cable Lock';
+SELECT * FROM Sales.SalesOrderDetail
+WHERE ProductID = 843;
+
+SELECT * FROM Production.Product
+ WHERE Name = 'Blade';
+SELECT * FROM Sales.SalesOrderDetail
+ WHERE ProductID = 316;
+
+SELECT * FROM Sales.SalesOrderDetail
+ WHERE ProductID = (SELECT ProductID
+                     FROM Production.Product
+					 WHERE Name = 'Cable Lock');
