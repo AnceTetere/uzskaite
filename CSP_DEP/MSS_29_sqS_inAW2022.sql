262127f022774c1ee912a726fe9d3739cb0918dd
AnceTetere <ance.tetere@outlook.com>
2025-04-15 12:43:38 +0300

    Modification to sQ.

--- CSP_DEP/MSS_29_sqS_inAW2022.sql
+++ CSP_DEP/MSS_29_sqS_inAW2022.sql
@@ -2,6 +2,30 @@ USE AdventureWorks2022
 GO
 
 SELECT * FROM Sales.SalesOrderDetail;
+SELECT * FROM Production.Product WHERE Name = 'Cable Lock';
+SELECT * FROM Sales.SalesOrderDetail WHERE ProductID = 843;
+
+SELECT * FROM Production.Product WHERE Name = 'Blade';
+SELECT * FROM Sales.SalesOrderDetail WHERE ProductID = 316;
+
+SELECT *
+FROM Sales.SalesOrderDetail
+WHERE ProductID = (SELECT ProductID
+                   FROM Production.Product
+				   WHERE Name = 'Blade');
+
+SELECT * FROM Sales.SalesOrderDetail
+WHERE ProductID = (SELECT ProductID
+                   FROM Production.Product
+				   WHERE Name = 'Cable Lock');
+
+SELECT COUNT(*) FROM Sales.SalesOrderDetail
+WHERE ProductID = (SELECT ProductID
+                   FROM Production.Product
+				   WHERE Name = 'Cable Lock');
+
+------*****-----
+SELECT * FROM Sales.SalesOrderDetail;
 SELECT * FROM Production.Product
 WHERE Name = 'Cable Lock';
 SELECT * FROM Sales.SalesOrderDetail
