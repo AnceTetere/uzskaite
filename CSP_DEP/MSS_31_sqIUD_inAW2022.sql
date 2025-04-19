ecf49222e0777d7a0a2e9f4dcafaa659b1831162
AnceTetere <ance.tetere@outlook.com>
2025-04-20 01:27:08 +0300

    Adding dates.

--- CSP_DEP/MSS_31_sqIUD_inAW2022.sql
+++ CSP_DEP/MSS_31_sqIUD_inAW2022.sql
@@ -1,6 +1,50 @@
 USE AdventureWorks2022
 GO
 
+CREATE TABLE Person.StateProvinceTest (
+   StateProvinceID int IDENTITY(1,1) Primary Key NOT NULL,
+   StateProvinceCode nchar(3) NOT NULL,
+   CountryRegionCode nvarchar(3) NOT NULL,
+   IsOnlyStateProvinceFlag dbo.Flag NOT NULL,
+   Name nvarchar(50) NOT NULL,
+   TerritoryID int NOT NULL,
+   ModifiedDate datetime NOT NULL)
+
+SELECT * FROM Person.StateProvinceTest; --empty table
+SELECT * FROM Person.StateProvince;     --181 rows
+
+INSERT INTO Person.StateProvinceTest
+	SELECT StateProvinceCode,
+	       CountryRegionCode,
+		   IsOnlyStateProvinceFlag,
+		   Name,
+		   TerritoryID,
+		   ModifiedDate
+	FROM Person.StateProvince;
+
+SELECT * FROM Person.StateProvinceTest;  --181 rows
+
+--Update table data with subqueries.
+--Change the TerritoryID of CA to 99.
+SELECT * --CountryRegionCode, TerritoryID
+FROM Person.StateProvinceTest
+WHERE CountryRegionCode = 'CA';
+
+UPDATE Person.StateProvinceTest 
+SET TerritoryID = 99
+WHERE CountryRegionCode IN (SELECT CountryRegionCode
+                           FROM Person.StateProvince
+						   WHERE CountryRegionCode = 'CA');
+
+SELECT * FROM Person.StateProvinceTest;
+
+--Delete data from the table with subqueries.
+DELETE FROM Person.StateProvinceTest
+WHERE CountryRegionCode IN (SELECT CountryRegionCode
+                            FROM Person.StateProvince
+							WHERE CountryRegionCode = 'CA');
+
+--------------------******************--------------------
 CREATE TABLE Person.StateProvinceTest(
    StateProvinceID int IDENTITY(1,1) Primary Key NOT NULL,
    StateProvinceCode nchar(3) NOT NULL,
