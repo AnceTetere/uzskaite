cc794098dd4b31e84047a258fec866dcb191a90f
AnceTetere <ance.tetere@outlook.com>
2025-05-06 16:30:46 +0300

    Developed schemas.

--- CSP_DEP/MSS_37_transactions.sql
+++ CSP_DEP/MSS_37_transactions.sql
@@ -1,3 +1,33 @@
+--SQL TRANSACTIONS
+USE AdventureWorks2022
+GO
+
+BEGIN TRAN
+	UPDATE Person.Person
+	SET FirstName = 'Terri' WHERE BusinessEntityID = 2
+
+ROLLBACK
+COMMIT
+
+BEGIN TRAN
+	UPDATE Person.Person
+	SET FirstName = 'Tom' WHERE BusinessEntityID = 2
+		SAVE TRAN SaveFirstName   --Save point for the first name
+	UPDATE Person.Person
+	SET MiddleName = 'Lee' WHERE BusinessEntityID = 2
+		SAVE TRAN SaveMiddleName  --Save point for the second name
+	UPDATE Person.Person
+	SET LastName = 'Duffy' WHERE BusinessEntityID = 2
+		SAVE TRAN SaveLastName    --Save point for the last name
+
+ROLLBACK Tran SaveMiddleName
+ROLLBACK
+COMMIT
+
+SELECT * FROM Person.Person
+
+
+---------------
 USE AdventureWorks2022
 GO
 
---
+++ CSP_DEP/MSS_38_schema.sql
@@ -0,0 +1,51 @@
+--Create Login and User
+CREATE LOGIN Test_User WITH PASSWORD = '7279';
+GO
+USE AdventureWorks2022;
+GO
+CREATE USER Test_User FOR LOGIN Test_User;
+
+--Create Schema
+CREATE SCHEMA TestSchema
+AUTHORIZATION Test_User
+
+--Create table in a schema
+CREATE TABLE Departments  --A table created in the default schema as dbo.Departments
+	(ID int Not Null,
+	  DepartmentName varchar Not Null)
+
+CREATE TABLE TestSchema.Departments --A table is created in the TestSchema as TestSchema.Departments
+   (ID INT Not Null,
+	DepartmentName varchar Not Null)
+
+--Change schema name of a table
+CREATE SCHEMA NewTestSchema
+
+ALTER SCHEMA NewTestSchema
+TRANSFER TestSchema.Departments
+
+--Drop a schema
+DROP SCHEMA TestSchema
+
+ALTER SCHEMA HumanResources
+TRANSFER NewTestSchema.Departments
+
+DROP SCHEMA NewTestSchema
+
+--Create view
+SELECT * FROM Person.Person
+SELECT * FROM Person.EmailAddress
+
+SELECT * FROM Person.Person AS P
+JOIN Person.EmailAddress AS E
+ON P.BusinessEntityID = E.BusinessEntityID
+
+CREATE VIEW VPersonWithEmailAddress AS
+	SELECT FirstName, LastName, EmailADdress
+	FROM Person.Person AS P
+	JOIN Person.EmailAddress AS E
+	ON P.BusinessEntityID = E.BusinessEntityID
+
+SELECT * FROM VPersonWithEmailAddress
+
+DROP VIEW VPersonWithEmailAddress
